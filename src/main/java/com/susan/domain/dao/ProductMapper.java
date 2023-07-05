f2b65aa8d024c5b140462549cdf6e191fe03288f
ajtwu1235 <ajtwu1235@gmail.com>
2023-07-05 21:39:46 +0900

    solve mybatis problem

---
+++ src/main/java/com/susan/domain/dao/ProductMapper.java
@@ -0,0 +1,9 @@
+package com.susan.domain.dao;
+
+import com.susan.domain.entity.Product;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductMapper {
+    Integer insertProduct(Product product);
+}
