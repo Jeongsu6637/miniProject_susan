9796de57801c419f1b1379ee7717fc58f481c068
khdaragon1102 <khdragon1102@gmail.com>
2023-07-06 16:08:48 +0900

    maincontroller 필요없는 테스트 코드 삭제

--- src/main/java/com/susan/controller/MainController.java
+++ src/main/java/com/susan/controller/MainController.java
@@ -10,16 +10,4 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
     //login 이동
-
-    @Autowired
-    public RedisTemplate<String, User> redisTemplate;
-    @GetMapping("/main")
-    public ModelAndView mainPageGet(ModelAndView mav){
-        User user = new User("test1", "1234", "서울", "테스트1", "010-1111-1111", 0);
-        redisTemplate.opsForValue().set("login_user", user);
-
-        user = redisTemplate.opsForValue().get("login_user");
-        System.out.println(user);
-        return mav;
-    }
 }
