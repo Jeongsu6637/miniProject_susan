f56547654518ec9d27fbe15882dd4ee2b060195d
khdaragon1102 <khdragon1102@gmail.com>
2023-07-06 14:00:22 +0900

    redis cache1

--- src/main/java/com/susan/controller/MainController.java
+++ src/main/java/com/susan/controller/MainController.java
@@ -1,6 +1,25 @@
 package com.susan.controller;
 
-public class MainController {
+import com.susan.domain.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class MainController {
     //login 이동
+
+    @Autowired
+    public RedisTemplate<String, User> redisTemplate;
+    @GetMapping("/main")
+    public ModelAndView mainPageGet(ModelAndView mav){
+        User user = new User("test1", "1234", "서울", "테스트1", "010-1111-1111", 0);
+        redisTemplate.opsForValue().set("login_user", user);
+
+        user = redisTemplate.opsForValue().get("login_user");
+        System.out.println(user);
+        return mav;
+    }
 }
