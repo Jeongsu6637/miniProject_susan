0a3feb872fdf28970e9c166102c1f1c491470b84
khdaragon1102 <khdragon1102@gmail.com>
2023-07-06 16:07:43 +0900

    로그인 캐시

--- src/main/java/com/susan/service/UserService.java
+++ src/main/java/com/susan/service/UserService.java
@@ -5,7 +5,10 @@ import com.susan.domain.entity.User;
 import com.susan.domain.request.IdFindRequest;
 import com.susan.domain.request.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService {
@@ -13,15 +16,35 @@ public class UserService {
 
     private final UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate<String, User> redisTemplate;
+
     public UserService(UserMapper userMapper) {
         this.userMapper = userMapper;
     }
 
     public User login(LoginRequest loginRequest) {
-        return userMapper.login(loginRequest);
+        User user = login_RedisCheck(loginRequest);
+        if (user == null) {
+            user = userMapper.login(loginRequest);
+            if (user != null)
+                login_RedisSetUser(user);
+            return user;
+        }
+        else
+            return user;
     }
 
     public User idFind(IdFindRequest idFindRequest) {
         return userMapper.idFind(idFindRequest);
+    }
+
+    private User login_RedisCheck(LoginRequest request){
+        User loginuser = redisTemplate.opsForValue().get("loginuser_" + request.getId());
+        return loginuser;
+    }
+
+    private void login_RedisSetUser(User user){
+        redisTemplate.opsForValue().set("loginuser_" + user.getId(), user, 1, TimeUnit.MINUTES);
     }
 }
